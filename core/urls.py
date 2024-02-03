74ffe3b6bae922056b75de94cf0cfdd973ab7b55
imanol <imanol@zenitsolar.eu>
2024-02-03 23:37:00 +0100

    api

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', include('pwa.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
