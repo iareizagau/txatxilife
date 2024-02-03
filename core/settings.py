b080499fca354848027d1ac15c8194a780b869ef
imanol <imanol@zenitsolar.eu>
2024-02-03 23:27:59 +0100

    api

--- core/settings.py
+++ core/settings.py
@@ -18,11 +18,9 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = False
 print("DEBUG", DEBUG)
-print("DB_HOST", os.getenv("DB_HOST"))
-print("DB_NAME", os.getenv("DB_NAME"))
-print("DB_USER", os.getenv("DB_USER"))
-print("DB_PORT", os.getenv("DB_PORT"))
+
 # load production server from .env
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
